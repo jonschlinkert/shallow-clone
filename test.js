60ad6be45f29e2f1f09ae8ddba73164ac5570ee0
jonschlinkert <jon.schlinkert@sellside.com>
2018-04-10 02:45:19 -0400

    adds support for more types
    - typed arrays
    - errors
    - array buffers
    - buffers
    - symbols

--- test.js
+++ test.js
@@ -5,54 +5,72 @@ var assert = require('assert');
 var clone = require('./');
 
 describe('clone()', function() {
-  it('should shallow clone an of primitives', function() {
-    assert.deepEqual(clone(['alpha', 'beta', 'gamma']), ['alpha', 'beta', 'gamma']);
+  describe('objects', function() {
+    it('should shallow clone an array of primitives', function() {
+      assert.deepEqual(clone(['alpha', 'beta', 'gamma']), ['alpha', 'beta', 'gamma']);
+    });
+
+    it('should shallow clone an array with varied elements', function() {
+      var val = [0, 'a', {}, [{}], [function() {}], function() {}];
+      assert.deepEqual(clone(val), val);
+    });
+
+    it('should clone Map', function() {
+      var a = new Map([[1, 5]]);
+      var b = clone(a);
+      a.set(2, 4);
+      assert.notDeepEqual(a, b);
+    });
+
+    it('should clone Set', function() {
+      var a = new Set([2, 1, 3]);
+      var b = clone(a);
+      a.add(8);
+      assert.notDeepEqual(a, b);
+    });
+
+    it('should shallow clone arrays', function() {
+      assert(clone([1, 2, 3]) !== [1, 2, 3]);
+      assert.deepEqual(clone([1, 2, 3]), [1, 2, 3]);
+    });
+
+    it('should shallow clone a regex', function() {
+      assert(clone(/foo/g) !== /foo/g);
+      assert.deepEqual(clone(/foo/g), /foo/g);
+    });
+
+    it('should shallow clone a date', function() {
+      const date = new Date();
+      assert(clone(date) !== date);
+      assert.deepEqual(clone(date), date);
+    });
+
+    it('should shallow clone objects', function() {
+      assert.deepEqual(clone({ a: 1, b: 2, c: 3 }), { a: 1, b: 2, c: 3 });
+    });
+
+    it('should shallow clone an array of objects.', function() {
+      var expected = [{ a: 0 }, { b: 1 }];
+      var actual = clone(expected);
+
+      assert(actual !== expected);
+      assert.deepEqual(actual, expected);
+      assert.deepEqual(actual[0], expected[0]);
+    });
   });
 
-  it('should shallow clone an array with varied elements', function() {
-    var val = [0, 'a', {}, [{}], [function() {}], function() {}];
-    assert.deepEqual(clone(val), val);
-  });
+  describe('primitives', function() {
+    it('should return primitives', function() {
+      assert.equal(clone(0), 0);
+      assert.equal(clone(1), 1);
+      assert.equal(clone('foo'), 'foo');
+    });
 
-  it('should return primitives', function() {
-    assert.equal(clone(0), 0);
-    assert.equal(clone('foo'), 'foo');
-  });
-
-  it('should deep clone Map', function() {
-    var a = new Map([[1, 5]]);
-    var b = clone(a);
-    a.set(2, 4);
-    assert.notDeepEqual(a, b);
-  });
-
-  it('should deep clone Set', function() {
-    var a = new Set([2, 1, 3]);
-    var b = clone(a);
-    a.add(8);
-    assert.notDeepEqual(a, b);
-  });
-
-  it('should shallow clone arrays', function() {
-    assert(clone([1, 2, 3]) !== [1, 2, 3]);
-    assert.deepEqual(clone([1, 2, 3]), [1, 2, 3]);
-  });
-
-  it('should shallow clone a regex', function() {
-    assert.deepEqual(clone(/foo/g), /foo/g);
-    assert(clone(/foo/g) !== /foo/g);
-  });
-
-  it('should shallow clone objects', function() {
-    assert.deepEqual(clone({a: 1, b: 2, c: 3 }), {a: 1, b: 2, c: 3 });
-  });
-
-  it('should shallow clone an array of objects.', function() {
-    var expected = [{ 'a': 0 }, { 'b': 1 }];
-    var actual = clone(expected);
-
-    assert(actual !== expected);
-    assert.deepEqual(actual, expected);
-    assert.deepEqual(actual[0], expected[0]);
+    it('should clone symbols', function() {
+      const val = { prop: Symbol() };
+      const cloned = clone(val);
+      assert.notEqual(cloned, val);
+      assert.equal(cloned.prop, val.prop);
+    });
   });
 });
